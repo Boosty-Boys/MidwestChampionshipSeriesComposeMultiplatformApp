b00d6639db1a581f6480fa27066236085b3e96e5
tommyjackson <tjackapps@gmail.com>
2023-10-25 01:30:46 -0500

    fix ios localdatetime format

--- shared/src/iosMain/kotlin/com.boostyboys.mcs/util/Format.kt
+++ shared/src/iosMain/kotlin/com.boostyboys.mcs/util/Format.kt
@@ -2,13 +2,19 @@ package com.boostyboys.mcs.util
 
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.toNSDateComponents
+import platform.Foundation.NSCalendar
+import platform.Foundation.NSCalendarIdentifierGregorian
+import platform.Foundation.NSDate
 import platform.Foundation.NSDateFormatter
 
 actual fun LocalDateTime.format(format: String): String {
+    val components = toNSDateComponents()
+    val calendar = NSCalendar(NSCalendarIdentifierGregorian)
+    val nsDate: NSDate = calendar.dateFromComponents(components)
+        ?: throw IllegalStateException("Could not convert Kotlin LocalDateTime to NSDate $this")
+
     val dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = format
-    return dateFormatter.stringFromDate(
-        toNSDateComponents().date
-            ?: throw IllegalStateException("Could not convert kotlin date to NSDate $this"),
-    )
+
+    return dateFormatter.stringFromDate(nsDate)
 }
